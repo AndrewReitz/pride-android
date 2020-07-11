2e5592ee732cb624500409574c9a2fcc143d30b3
Andrew Reitz <aj.reitz@gmail.com>
2020-07-10 20:08:32 -0500

    Setup for Per Updates
    Upgraded Parse
    Added Room
    Added Coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
     google()
     mavenCentral()
     jcenter()
-    maven { url = uri("https://jitpack.io") }
+    maven("https://jitpack.io")
 }
 
 android {
@@ -91,6 +91,8 @@ android {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.7")
 
     implementation("androidx.core:core-ktx:1.3.0")
     implementation("androidx.appcompat:appcompat:1.1.0")
@@ -98,6 +100,10 @@ dependencies {
     implementation("androidx.emoji:emoji-bundled:1.1.0")
     implementation("androidx.browser:browser:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+
+    implementation("androidx.room:room-runtime:2.2.5")
+    kapt("androidx.room:room-compiler:2.2.5")
+    implementation("androidx.room:room-ktx:2.2.5")
 
     implementation("com.google.android.material:material:1.1.0")
 
@@ -117,7 +123,7 @@ dependencies {
     implementation("com.jakewharton.rxbinding2:rxbinding-appcompat-v7:2.2.0")
     implementation("com.f2prateek.rx.preferences2:rx-preferences:2.0.0")
 
-    implementation("com.parse:parse-android:1.17.3")
+    implementation("com.github.parse-community.Parse-SDK-Android:parse:1.24.2")
 
     implementation("com.github.bumptech.glide:glide:4.11.0")
     kapt("com.github.bumptech.glide:compiler:4.11.0")
